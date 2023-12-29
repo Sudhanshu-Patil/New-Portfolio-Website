83ac8c89d29005a455793943684c32e1fd48910f
Sudhanshu Patil <93507588+Sudhanshu-Patil@users.noreply.github.com>
2023-12-29 15:29:17 +0530

    Add files via upload

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Sudhanshu Patil',
+    title: "Web Developer / Data Analyst",
+    desciption:"There wasn't a bird in the sky, but that was not what caught his attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. He knew what was coming and he hoped he was prepared. ",
+    image: 'https://media.licdn.com/dms/image/D5603AQHrZ3iM_HfetQ/profile-displayphoto-shrink_800_800/0/1703691367819?e=1709164800&v=beta&t=Od0_t8YATtqCnfXDZHfqOYC2xYF9_ItgeG9pC4p1kiw',
+    resumePdf: resume
+}
