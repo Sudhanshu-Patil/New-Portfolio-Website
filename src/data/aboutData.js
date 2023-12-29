cc11e7795e92d95f24fd391444659285d975c7ef
Sudhanshu Patil <93507588+Sudhanshu-Patil@users.noreply.github.com>
2023-12-29 20:15:31 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Sudhanshu. I'm a Web Designer, Developer and Data Analyst.",
-    description2: "I am currently persuing B.E. in ECE from BITS-Pilani, Hyderabad Campus. I love problem solving and I'm actively looking for SDE roles ",
+    description1: "My name is Sudhanshu. I'm a Web Designer, Developer and Data Analyst.",
+    description2: "I am currently persuing B.E. in ECE from BITS-Pilani, Hyderabad Campus. I love problem solving and I'm actively looking for SDE roles.",
     image: 1
 }
