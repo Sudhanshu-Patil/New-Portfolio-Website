2f16008bac98730e646d66a5a877e0d1fb927cb7
Sudhanshu Patil <93507588+Sudhanshu-Patil@users.noreply.github.com>
2023-12-29 15:32:24 +0530

    Delete data directory

--- data/aboutData.js
+++
@@ -1,6 +0,0 @@
-export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Sudhanshu. I'm a Web Designer, Developer and Data Analyst.",
-    description2: "I am currently persuing B.E. in ECE from BITS-Pilani, Hyderabad Campus. I love problem solving and I'm actively looking for SDE roles ",
-    image: 1
-}
--- data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'sudhanshupatil588@gmail.com',
-    phone: '+917248913660 / +917038913660',
-    address: 'Pusad, Maharashtra, India - 445204 ',
-
-    sheetAPI: ''
-}
